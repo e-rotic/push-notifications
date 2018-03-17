d0112dbdb7530be5ba0c3ef75f4c2c83d86a698e
unknown <erotic90.shy@gmail.com>
2018-03-17 14:05:16 -0500

    first commit

---
+++ src/main/java/com/erotic/pushnotifications/PushNotificationsApplication.java
@@ -0,0 +1,12 @@
+package com.erotic.pushnotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushNotificationsApplication.class, args);
+	}
+}
